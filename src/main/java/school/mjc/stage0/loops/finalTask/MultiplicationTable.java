9422598c4d1047512dfe21f67fab64d8d8c496b4
Sandro Suqnidze <sandrosuqnidze2@gmail.com>
2023-09-11 20:10:43 +0400

    4.2 Final Task

--- src/main/java/school/mjc/stage0/loops/finalTask/MultiplicationTable.java
+++ src/main/java/school/mjc/stage0/loops/finalTask/MultiplicationTable.java
@@ -2,5 +2,13 @@ package school.mjc.stage0.loops.finalTask;
 
 public class MultiplicationTable {
     public void printTable(int numberTableToPrint){
+        if (numberTableToPrint < 0) {
+        } else {
+            for (int i = 0; i <= 10; i++) {
+                int sum = i * numberTableToPrint;
+                String result = i + " x " + numberTableToPrint + " = " + sum;
+                System.out.println(result);
+            }
+        }
     }
 }
